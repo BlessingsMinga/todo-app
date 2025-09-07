0509d2b81e0208e1038eb7688cb22a253597871b
BlessingsMinga <blessingsminga11@gmail.com>
2025-09-07 15:52:02 +0200

    Add initial server and view setup for TODO list application

--- server.js
+++ server.js
@@ -6,4 +6,28 @@ const app = express();
 const PORT = 3000;
 
 
-app.set("view engine")
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let tasks = [];
+
+app.get("/", (req, res) => {
+    res.render("index", { tasks });
+});
+
+
+app.post("/add", (req, res) => {
+    let task = req.body.task;
+    if (task) tasks.push(task);
+    res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+    const index = req.body.index;
+    tasks.splice(index, 1);
+    res.redirect("/");
+});
+
+app.listen(PORT, () => 
+    console.log(`Server is running on http://localhost:${PORT}`));
